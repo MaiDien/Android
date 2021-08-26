f92d37ede5b7be508831f210ffd8a3c139ebd2b2
MaiDien <42720252+MaiDien@users.noreply.github.com>
2021-08-27 04:54:46 +0700

    Add files via upload

---
+++ Bỏ qua thời gian chờ.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name BỎ QUA THỜI GIAN CHỜ
+// @description bỏ qua thời gian chờ với link rút gọn quảng cáo,...
+// @version 1.0
+// @include áp dụng cho 123link
+// @author MaiDien
+// @match https://www.123link.*/*
+// @downloadURL 
+// ==/UserScript==
+
+app_vars['counter_value']='0';
